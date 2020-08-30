2bfacef05aa34b10ff95405056e8874ba9f1f376
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-08-30 17:17:02 +1000

    added some comments

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ function App() {
           path="/"
           render={() => {
             return (
+              // If there's a currentUser, redirect to HomeWithRouter
+              // else if there's no currentUser, redirect to SignIn
               <Redirect to={hamburgerConstants.HOME} /> 
             )
           }}
         />
 
+        {/* this home should be a privateRoute so it can't be accessed without a currentUser */}
         <Route exact path={hamburgerConstants.HOME} component={HomeWithRouter} />
         <PrivateRoute path={hamburgerConstants.PROFILE} component={Profile} />
 
